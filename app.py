baad7cdb227a8395c71d30915f33d91609392523
johnsliao <john@johnliao.org>
2019-03-22 16:22:47 -0400

    WIP

--- app.py
+++ app.py
@@ -1,40 +1,54 @@
-import sys, pygame
+import sys, pygame, random
 
 pygame.init()
 
 size = width, height = 750, 1000
 v_x, v_y = 0, 0
-black = 0, 0, 0
+black = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
+ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
 clock = pygame.time.Clock()
 ballrect.bottom = height
 t = 0
 
 pressed_left = pressed_right = False
+bells = []
+
+
+class Bell(object):
+    def __init__(self):
+        self.image = pygame.image.load("ball.gif")
+        self.x = random.randint(0, width - 250)
+        self.y = random.randint(height / 4, height)
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+
+bells = [Bell() for x in range(5)]
 
 while True:
     ballrect = ballrect.move([v_x, v_y])
 
     if ballrect.bottom > height:
-        # Hit ground
         ballrect.bottom = height
         v_y = t = 0
 
-    print('v_y: {}, t: {}'.format(v_y, t))
-
     if v_y != 0:
-        v_y = v_y + .015 * t * t
+        delta = + .015 * t * t
+        v_y = v_y + delta
+        for bell in bells:
+            bell.y += v_y + delta
         t += 1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and v_y == 0:
                 v_y = -40
 
         if event.type == pygame.KEYDOWN:
@@ -55,5 +69,7 @@ while True:
 
     screen.fill(black)
     screen.blit(ball, ballrect)
+    for bell in bells:
+        bell.draw(screen)
     pygame.display.update()
     clock.tick(100)
